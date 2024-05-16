60650cc7b34b7933df86d90422876aa078e937c0
PHTLing <145812848+PHTLing@users.noreply.github.com>
2024-05-17 04:39:31 +0700

    hihi fix bug

--- Routes/adminRoutes.py
+++ Routes/adminRoutes.py
@@ -9,25 +9,25 @@ def initRoutes(app, mysql):
         CCCD = request.form['CCCD']
         GioiTinh = request.form['GioiTinh']
         NgayGioSinh = request.form['NgayGioSinh']
-        QueQuan = request.form['QueQuan']
-        NgheNghiep = request.form['NgheNghiep']
+        MaQueQuan = request.form['MaQueQuan']
+        MaNgheNghiep = request.form['MaNgheNghiep']
         SDT = request.form['SDT']
         DiaChi = request.form['DiaChi']
         password = request.form['password']
+        id_tvc = request.form['id_tvc'] or None
+        MaQuanHe = request.form['MaQuanHe'] or None
+        NgayPhatSinh = request.form['NgayPhatSinh'] or None
         #Connect database
         cur = mysql.connection.cursor()
         cur.execute(
-            'INSERT INTO THANHVIEN (HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,Sđt,DiaChi) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)', HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,SDT,DiaChi
+            'INSERT INTO THANHVIEN (HoTen,CCCD,GioiTinh,NgayGioSinh,MaQueQuan,MaNgheNghiep,SDT,DiaChi,id_tvc,MaQuanHe,NgayPhatSinh) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',[ HoTen,CCCD,GioiTinh,NgayGioSinh,MaQueQuan,MaNgheNghiep,SDT,DiaChi,id_tvc,MaQuanHe,NgayPhatSinh]
         )
         cur.execute(
-            'INSERT INTO  USERS (cccd,password) VALUES (%s,%s)', CCCD,password
+            'INSERT INTO  USERS (CCCD,password,role) VALUES (%s,%s,user)', [CCCD,password]
             )
         #results = cur.fetchall()
+        mysql.connection.commit()
         cur.close()
-        
-        # print(results[0]['email'])
-        # #xử lý kết quả trước khi trả về
-        # Trả về kết quả dưới dạng JSON
         return jsonify('Đăng ký thành công!')
 
     @app.route('/admin/SignIn', methods=['POST'])
@@ -35,12 +35,12 @@ def initRoutes(app, mysql):
         cccd = request.form['CCCD']
         password = request.form['password']
 
-        if (cccd.size() !=12 ):
+        if (len(cccd) !=12 ):
             return jsonify({'EC': '2', 'EM': 'CCCD không hợp lệ'}), 400
         
         cur = mysql.connection.cursor()
         cur.execute(
-            'SELECT * FROM USERS WHERE CCCD = %s ', cccd
+            'SELECT * FROM USERS WHERE CCCD = %s ', [cccd]
         )
         results = cur.fetchall()
         if (password != results[0]['password']):
@@ -48,20 +48,25 @@ def initRoutes(app, mysql):
         cur.close()
         return jsonify({'EC': '0', 'EM':'Success'}),200
     
-    @app.route('/admin/Account', methods=['GET'])
-    def admin_Account():
-        id = request.args.get('id')
+    @app.route('/admin/Account/<int:id>', methods=['GET'])
+    def admin_Account(id):
+        print(id)
+        #id = request.args.get('id')
         cur = mysql.connection.cursor()
         cur.execute(
-            'SELECT * FROM THANHVIEN WHERE id = %s', id
+            'SELECT * FROM THANHVIEN INNER JOIN USERS ON THANHVIEN.id_user = USERS.id WHERE id_user = %s', [id]
         )
         results = cur.fetchall()
+        for item in results:
+        # Sử dụng phương thức pop()
+             item.pop("password" , None)
+             item.pop("USERS.id" , None)
+            #del results['password']
         cur.close()
         return jsonify(results)
     
-    @app.route('/admin/Home', methods=['POST'])
+    @app.route('/admin/Home/', methods=['GET'])
     def admin_Home():
-        id = request.args.get('id')
         cur = mysql.connection.cursor()
         cur.execute(
             'SELECT * FROM THANHVIEN ', 
@@ -73,46 +78,43 @@ def initRoutes(app, mysql):
     @app.route('/admin/SearchMember', methods=['GET','POST'])
     def admin_SearchMember():
         results = []
-        HoTen = request.form['HoTen']
-        NamSinh = request.form['NamSinh']
-        QueQuan = request.form['QueQuan']
-        DiaChi = request.form['DiaChi']
-        query = "SELECT * FROM ThanhVien WHERE 1=1"
+        HoTen = request.form['HoTen'] 
+        NamSinh = request.form['NamSinh'] 
+        QueQuan = request.form['QueQuan'] 
+        DiaChi = request.form['DiaChi'] 
+        query = "SELECT * FROM THANHVIEN WHERE 1=1"
         params = []
-
-        if HoTen:
-            query += " AND HoTen LIKE ?"
+        if HoTen  != 'null':
+            query += " AND HoTen LIKE %s"
             params.append(f"%{HoTen}%")
-        if NamSinh:
-            try:
+        if NamSinh != 'null':
                 NamSinh = int(NamSinh)
-                query += " AND NamSinh = ?"
+                query += " AND YEAR(NgayGioSinh) = %s"
                 params.append(NamSinh)
-            except ValueError:
-                return jsonify({'EC': '3', 'EM': 'Năm sinh không hợp lệ'}), 400
-        if QueQuan:
-            query += " AND QueQuan LIKE ?"
+        if QueQuan  != 'null':
+            query += " AND QueQuan LIKE %s"
             params.append(f"%{QueQuan}%")
-        if DiaChi:
-            query += " AND DiaChi LIKE ?"
+        if DiaChi != 'null':
+            query += " AND DiaChi LIKE %s"
             params.append(f"%{DiaChi}%")
 
         cur =  mysql.connection.cursor()
         cur.execute(query, params)
-        rows = cur.fetchall()
-        if not rows:
+        temp = cur.fetchall()
+        print(temp)
+        if not temp:
             return jsonify({'EC': '2', 'EM': 'Không tìm thấy người dùng nào'}), 404
 
-    results = []
-    for row in rows:
-        results.append({
-            'name': row['name'],
-            'birth_year': row['birth_year'],
-            'hometown': row['hometown'],
-            'address': row['address']
-        })
+        results = []
+        for row in temp:
+            results.append({
+                'HoTen': row['HoTen'],
+                'NamSinh': row['NamSinh'],
+                'QueQuan': row['QueQuan'],
+                'DiaChi': row['DiaChi']
+            })
 
-    return jsonify({'EC': '0', 'EM': 'Thành công', 'data': results}), 200
+        return jsonify({'EC': '0', 'EM': 'Thành công', 'data': results}), 200
 
 
-    @app.route('/admin/UpdateMember', methods=['POST'])
+   
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DB'] = 'ucpc_register'
+app.config['MYSQL_DB'] = 'se_backend'
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
