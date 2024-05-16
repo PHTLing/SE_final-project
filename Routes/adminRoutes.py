3641d697602a28597987048202ee2ebe6bd5a17e
PHTLing <145812848+PHTLing@users.noreply.github.com>
2024-05-16 09:09:57 +0700

    hihi

--- Routes/adminRoutes.py
+++ Routes/adminRoutes.py
@@ -2,21 +2,31 @@ from flask import jsonify, request
 
 def initRoutes(app, mysql):
 
-    @app.route('/admin', methods=['POST'])
+    @app.route('/admin/SignUp', methods=['POST'])
     def admin():
         #Lấy dữ liệu từ database
-        id = request.form['id']
+        HoTen = request.form['HoTen']
+        CCCD = request.form['CCCD']
+        GioiTinh = request.form['GioiTinh']
+        NgayGioSinh = request.form['NgayGioSinh']
+        QueQuan = request.form['QueQuan']
+        NgheNghiep = request.form['NgheNghiep']
+        Sđt = request.form['Sđt']
+        DiaChi = request.form['DiaChi']
+        password = request.form['password']
+        #Connect database
         cur = mysql.connection.cursor()
         cur.execute(
-            'SELECT * FROM users WHERE id = %s', (id,)  
+            'INSERT INTO THANHVIEN (HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,Sđt,DiaChi) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)', HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,Sđt,DiaChi
+            'INSERT INTO  USERS (cccd,password) VALUES (%s,%s)', CCCD,password
             )
-        results = cur.fetchall()
+        #results = cur.fetchall()
         cur.close()
         
-        print(results[0]['email'])
-        #xử lý kết quả trước khi trả về
+        # print(results[0]['email'])
+        # #xử lý kết quả trước khi trả về
         # Trả về kết quả dưới dạng JSON
-        return jsonify(results[0])
+        return jsonify('Đăng ký thành công!')
 
 
     
