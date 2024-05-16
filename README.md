a847c2fa44b0a12d006764737e1b52823ac3ee71
PHTLing <145812848+PHTLing@users.noreply.github.com>
2024-05-17 00:47:25 +0700

    hihi

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 #khởi động mtr
 venv\Scripts\activate
+Chạy dô file requirements.txt để copy trước nha, phòng hờ
+
 #dùng lênh sau để tạo txt file chứa các thư viện cần cài đặt
 ```
-pip freeze > requirements.txt
+pip freeze > requirements.txt  /// check thử coi trong ẻm có nội dung chưa
 ```
-
 #dùng lệnh sau để cài đặt các thư viện trong file requirements.txt
 ```
 pip install -r requirements.txt
 ```
 #dùng lệnh sau để chạy chương trình
 ```
-flask run
+flask run --debug
 ```
--- Routes/adminRoutes.py
+++ Routes/adminRoutes.py
@@ -3,7 +3,7 @@ from flask import jsonify, request
 def initRoutes(app, mysql):
 
     @app.route('/admin/SignUp', methods=['POST'])
-    def admin():
+    def admin_SignUp():
         #Lấy dữ liệu từ database
         HoTen = request.form['HoTen']
         CCCD = request.form['CCCD']
@@ -11,13 +11,15 @@ def initRoutes(app, mysql):
         NgayGioSinh = request.form['NgayGioSinh']
         QueQuan = request.form['QueQuan']
         NgheNghiep = request.form['NgheNghiep']
-        Sđt = request.form['Sđt']
+        SDT = request.form['SDT']
         DiaChi = request.form['DiaChi']
         password = request.form['password']
         #Connect database
         cur = mysql.connection.cursor()
         cur.execute(
-            'INSERT INTO THANHVIEN (HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,Sđt,DiaChi) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)', HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,Sđt,DiaChi
+            'INSERT INTO THANHVIEN (HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,Sđt,DiaChi) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)', HoTen,CCCD,GioiTinh,NgayGioSinh,QueQuan,NgheNghiep,SDT,DiaChi
+        )
+        cur.execute(
             'INSERT INTO  USERS (cccd,password) VALUES (%s,%s)', CCCD,password
             )
         #results = cur.fetchall()
@@ -28,5 +30,89 @@ def initRoutes(app, mysql):
         # Trả về kết quả dưới dạng JSON
         return jsonify('Đăng ký thành công!')
 
+    @app.route('/admin/SignIn', methods=['POST'])
+    def admin_SignIn ():
+        cccd = request.form['CCCD']
+        password = request.form['password']
 
+        if (cccd.size() !=12 ):
+            return jsonify({'EC': '2', 'EM': 'CCCD không hợp lệ'}), 400
+        
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'SELECT * FROM USERS WHERE CCCD = %s ', cccd
+        )
+        results = cur.fetchall()
+        if (password != results[0]['password']):
+            return jsonify({'EC': '1', 'EM':'Mật khẩu không đúng'}),401
+        cur.close()
+        return jsonify({'EC': '0', 'EM':'Success'}),200
     
+    @app.route('/admin/Account', methods=['GET'])
+    def admin_Account():
+        id = request.args.get('id')
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'SELECT * FROM THANHVIEN WHERE id = %s', id
+        )
+        results = cur.fetchall()
+        cur.close()
+        return jsonify(results)
+    
+    @app.route('/admin/Home', methods=['POST'])
+    def admin_Home():
+        id = request.args.get('id')
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'SELECT * FROM THANHVIEN ', 
+        )
+        results = cur.fetchall()
+        cur.close()
+        return jsonify(results)
+    
+    @app.route('/admin/SearchMember', methods=['GET','POST'])
+    def admin_SearchMember():
+        results = []
+        HoTen = request.form['HoTen']
+        NamSinh = request.form['NamSinh']
+        QueQuan = request.form['QueQuan']
+        DiaChi = request.form['DiaChi']
+        query = "SELECT * FROM ThanhVien WHERE 1=1"
+        params = []
+
+        if HoTen:
+            query += " AND HoTen LIKE ?"
+            params.append(f"%{HoTen}%")
+        if NamSinh:
+            try:
+                NamSinh = int(NamSinh)
+                query += " AND NamSinh = ?"
+                params.append(NamSinh)
+            except ValueError:
+                return jsonify({'EC': '3', 'EM': 'Năm sinh không hợp lệ'}), 400
+        if QueQuan:
+            query += " AND QueQuan LIKE ?"
+            params.append(f"%{QueQuan}%")
+        if DiaChi:
+            query += " AND DiaChi LIKE ?"
+            params.append(f"%{DiaChi}%")
+
+        cur =  mysql.connection.cursor()
+        cur.execute(query, params)
+        rows = cur.fetchall()
+        if not rows:
+            return jsonify({'EC': '2', 'EM': 'Không tìm thấy người dùng nào'}), 404
+
+    results = []
+    for row in rows:
+        results.append({
+            'name': row['name'],
+            'birth_year': row['birth_year'],
+            'hometown': row['hometown'],
+            'address': row['address']
+        })
+
+    return jsonify({'EC': '0', 'EM': 'Thành công', 'data': results}), 200
+
+
+    @app.route('/admin/UpdateMember', methods=['POST'])
