a2b223f41cceade06160fa67033ea441b79701f6
PHTLing <145812848+PHTLing@users.noreply.github.com>
2024-05-19 03:39:06 +0700

    update

--- Routes/adminRoutes.py
+++ Routes/adminRoutes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import jsonify, request, session
 from datetime import datetime
 
 def initRoutes(app, mysql):
@@ -66,12 +66,22 @@ def initRoutes(app, mysql):
             'SELECT tv.HoTen, tv.id, u.role FROM THANHVIEN tv JOIN USERS u ON tv.id_user=u.id WHERE tv.CCCD = %s', [cccd]
         )
         results = cur.fetchall()
+        session['cccd'] = cccd
         # Gửi id
         return jsonify({'EC': '0', 'EM':'Success', 'data': results}),200
     
+    @app.route('/admin/SignOut/<int:id>', methods=['GET'])
+    def admin_SignOut(id):
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'SELECT id_user FROM THANHVIEN WHERE id = %s', [id]
+        )
+        results = cur.fetchall()
+        session.pop('result', None)
+        return jsonify({'EC': '0', 'EM':'Đăng xuất thành công'}),200
+
     @app.route('/admin/Account/<int:id>', methods=['GET'])
     def admin_Account(id):
-        print(id)
         #id = request.args.get('id')
         cur = mysql.connection.cursor()
         cur.execute(
@@ -84,8 +94,30 @@ def initRoutes(app, mysql):
              item.pop("USERS.id" , None)
             #del results['password']
         cur.close()
-        return jsonify(results)
+        return jsonify({'EC':0, 'EM':'Success', 'data':results})
     
+    @app.route('/admin/ChangeAvt/<int:id>', methods=['POST'])
+    def admin_ChangeAvt(id):
+        url = request.form['url']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'UPDATE THANHVIEN SET Avt = %s WHERE id = %s', [url,id]
+        )
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'EC':0, 'EM':'Change avatar successed!'}),200
+    
+    @app.route('/admin/ChangeSDT/<int:id>', methods=['POST'])
+    def admin_ChangeSDT(id):
+        SDT = request.form['SDT']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'UPDATE THANHVIEN SET SDT = %s WHERE id = %s', [SDT,id]
+        )
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'EC':0, 'EM':'Change phone number successed!'}),200
+
     @app.route('/admin/Home/', methods=['GET'])
     def admin_Home():
         cur = mysql.connection.cursor()
@@ -96,19 +128,8 @@ def initRoutes(app, mysql):
         cur.close()
         return jsonify(results)
     
-    @app.route('/admin/Search', methods=['POST'])
+    @app.route('/admin/SearchMember', methods=['POST'])
     def admin_SearchMember():
-        #Gửi bảng quê quán
-        cur =  mysql.connection.cursor()
-        cur.execute(
-            'SELECT * FROM QUEQUAN',
-        )
-        results = cur.fetchall()
-        cur.close()
-        return jsonify(results)
-    
-    @app.route('/admin/SearchMember', methods=['GET','POST'])
-    def admin_Search():
         results = []
         #Lấy dữ liệu nhập
         HoTen = request.form['HoTen'] 
@@ -247,7 +268,6 @@ def initRoutes(app, mysql):
             return jsonify({'EC': '1', 'EM': 'Thông tin không chính xác'}), 404
         return jsonify({'EC': '0', 'EM': 'Success;', 'data': results}), 200
 
-
     @app.route('/admin/Add', methods=['POST'])
     def admin_Add(id):
         cur = mysql.connection.cursor()
@@ -283,3 +303,38 @@ def initRoutes(app, mysql):
         mysql.connection.commit()
         cur.close()
         return jsonify('Thêm thành viên thành công!')
+    
+    @app.route('/admin/SuggestName', methods=['POST'])
+    def admin_SuggestName():
+        results = []
+        HoTen = request.form['HoTen']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'SELECT id, HoTen, NgayGioSinh, GioiTinh FROM THANHVIEN WHERE HoTen LIKE %s', [f"%{HoTen}%"]
+        )
+        results = cur.fetchall()
+        results[0]['NgayGioSinh'] = results[0]['NgayGioSinh'].strftime('%Y-%m-%d')
+        cur.close()
+        return jsonify({'EC':'0','EM':'Found!','data':results})
+
+    @app.route('/admin/RecordDeath', methods=['POST'])
+    def admin_RecordDeath():
+        MaDiaDiemMaiTang = request.form['MaDiaDiemMaiTang']
+        MaNguyenNhan = request.form['MaNguyenNhan']
+        id = request.form['id']
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'SELECT id,HoTen,GioiTinh,NgayGioSinh FROM THANHVIEN WHERE id = %s', [id]
+        )
+        results = cur.fetchone()
+        if results is None:
+            return jsonify({'EC': '1', 'EM': 'Không tìm thấy thành viên'}), 404
+        NgayGioMat = request.form['NgayGioMat']
+        cur.execute(
+            'UPDATE THANHVIEN SET Status = 1 WHERE id = %s', [id]
+        )
+        cur.execute(
+            'INSERT INTO KETTHUC (MaThanhVien,NgayGioMat,MaNguyenNhan, MaDiaDiemMaiTang) VALUES (%s,%s,%s,%s)',[id,NgayGioMat,MaNguyenNhan,MaDiaDiemMaiTang]
+        )
+        mysql.connection.commit()
+        return jsonify({'EC': '0', 'EM':'Ghi nhận thành công'}), 200
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from Routes.adminRoutes import initRoutes as routes 
 app = Flask(__name__)
-
+app.secret_key = 'SEProject' 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DB'] = 'se_backend'
